f48789ecad5d8838b46050c2d3875f4ed691beb1
topwebghost <ivanfridrich1981129@yandex.com>
2017-11-05 21:22:20 +0100

    add config

---
+++ src/config.js
@@ -0,0 +1,29 @@
+// EXAMPLE ONLY! THIS FILE IS USUALLY NOT PART OF GIT TRACKING
+// .gitignore skips this at the project level, but it is added for example here
+/**
+ * NOTE: This file is ignored from git tracking. In a CI environment, it is
+ * generated using build/create-config.js by calling npm run create-config (or
+ * using firebase-ci if deploying to Firebase hosting). This is done so that
+ * environment specific settings can be applied.
+ */
+
+export const env = 'development'
+
+// Config for firebase
+export const firebase = {
+  apiKey: 'AIzaSyBFYDRD_kp60p9e9Zpfn42YX3-FvTgBTus',
+  authDomain: 'react-redux-firebase-exa-e18e5.firebaseapp.com',
+  databaseURL: 'react-redux-firebase-exa-e18e5.firebaseio.com',
+  storageBucket: 'react-redux-firebase-exa-e18e5.appspot.com'
+}
+
+// Config for react-redux-firebase
+// For more details, visit https://prescottprue.gitbooks.io/react-redux-firebase/content/config.html
+export const reduxFirebase = {
+  userProfile: "users", // root that user profiles are written to
+  enableLogging: false, // enable/disable Firebase Database Logging
+  updateProfileOnLogin: false // enable/disable updating of profile on login
+  // profileDecorator: (userData) => ({ email: userData.email }) // customize format of user profile
+}
+
+export default { env, firebase, reduxFirebase }
